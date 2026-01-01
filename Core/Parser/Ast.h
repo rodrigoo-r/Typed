366595782825f9b13a13c748b6eb08bb9fe30aaa
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 14:11:48 -0500

    feat: add AST structure definition in Ast.h for the Zelix programming language

---
+++ Core/Parser/Ast.h
@@ -0,0 +1,48 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#pragma once
+#include <Celery/Array/Vector.h>
+
+namespace Typed::Core::Parser
+{
+    struct AST
+    {
+        enum class Rule
+        {
+            Root,
+            Procedure,
+            Identifier,
+            Embed,
+            TextReplace,
+            StringLiteral,
+            End,
+            Foreach,
+            In,
+            Pull,
+            From,
+            Block
+        };
+
+        Rule rule;
+        Celery::Array::Vector<AST*> children;
+    };
+}
