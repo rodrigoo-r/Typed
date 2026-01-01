a5e6e5e6a96a4731a5f59f0dc3c37bbc7448d2fc
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 11:20:13 -0500

    add FlushToken.h to implement token flushing in the Lexer

---
+++ Environment/Lexer/FlushToken.h
@@ -0,0 +1,56 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#pragma once
+
+#include "State.h"
+#include <celery/string/string.h>
+
+#include "Lexer.h"
+#include "Token.h"
+
+namespace Typed::Environment::Lexer
+{
+    inline void FlushToken(
+        const Celery::Str::String &source,
+        TokenStream &stream,
+        const Token::Type type,
+        State &state
+    )
+    {
+        // No token to flush
+        if (state.Len == 0)
+        {
+            return;
+        }
+
+        auto source_ptr = source.Ptr() + state.Start;
+
+        Token token;
+        token.type = type;
+        token.value = Celery::Str::External(source_ptr, state.Len);
+
+        stream.PushBack(token);
+
+        // Reset state
+        state.Len = 0;
+    }
+}
