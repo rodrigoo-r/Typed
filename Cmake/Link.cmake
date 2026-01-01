6e540f3216dd35ebc5d7be6f09c4ce20b3f8baa6
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 10:51:56 -0500

    refactor Link.cmake to simplify target_include_directories and improve readability

--- Cmake/Link.cmake
+++ Cmake/Link.cmake
@@ -1,14 +1,10 @@
 # ============================================================================ #
 # Executable & Libraries
 # ============================================================================ #
-target_include_directories(
-        ${PROJECT_NAME}
-        PRIVATE
-        Core
-)
 
 # Link libraries
-target_link_libraries(${PROJECT_NAME}
+target_link_libraries(
+        ${PROJECT_NAME}
         PRIVATE
         Celery::Celery
         unordered_dense::unordered_dense
