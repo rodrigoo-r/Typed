9a565f74bbf899f610e5642fdb1ffcb02f5a09c4
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 18:21:41 -0500

    feat: add Get function for retrieving Symbol::Unit from AST

---
+++ Runtime/Unit/Get.cpp
@@ -0,0 +1,51 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#include "Get.h"
+
+#include <Celery/Io/Io.h>
+
+#include "Rule/Procedure/Procedure.h"
+
+using namespace Typed::Runtime;
+
+Symbol::Unit Unit::Get(Core::Parser::AST *root)
+{
+    Symbol::Unit unit;
+    for (auto &child : root->Children)
+    {
+        switch (child->Kind)
+        {
+            case Core::Parser::AST::Rule::Procedure:
+            {
+                Rule::Procedure(unit, child);
+                break;
+            }
+
+            default:
+            {
+                break;
+            }
+        }
+    }
+
+    return unit;
+}
