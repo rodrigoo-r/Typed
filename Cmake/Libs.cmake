8509c081a856c8c91b26cf6f34c7da07a448ed7b
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 18:48:46 -0500

    feat: add magic_enum dependency and update linking in CMake files

--- Cmake/Libs.cmake
+++ Cmake/Libs.cmake
@@ -14,6 +14,11 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/rodrigoo-r/Celery.git
         GIT_TAG        master
 )
+FetchContent_Declare(
+        magic_enum
+        GIT_REPOSITORY https://github.com/Neargye/magic_enum
+        GIT_TAG v0.9.7
+)
 
 if(NOT APPLE)
     FetchContent_Declare(
@@ -21,6 +26,8 @@ if(NOT APPLE)
             GIT_REPOSITORY https://github.com/Cyan4973/xxHash
             GIT_TAG        v0.8.3
     )
+
+    FetchContent_MakeAvailable(xxhash)
 endif()
 
-FetchContent_MakeAvailable(unordered_dense Celery)
+FetchContent_MakeAvailable(unordered_dense Celery magic_enum)
--- Cmake/Link.cmake
+++ Cmake/Link.cmake
@@ -2,20 +2,16 @@
 # Executable & Libraries
 # ============================================================================ #
 
+if (APPLE) # macOS links xxhash statically, so no need to link it again
+    target_link_directories(${PROJECT_NAME} PRIVATE /opt/homebrew/opt/xxhash/lib)
+endif ()
+
 # Link libraries
 target_link_libraries(
         ${PROJECT_NAME}
         PRIVATE
         Celery::Celery
         unordered_dense::unordered_dense
-)
-
-if (APPLE) # macOS links xxhash statically, so no need to link it again
-    target_link_directories(${PROJECT_NAME} PRIVATE /opt/homebrew/opt/xxhash/lib)
-endif ()
-
-target_link_libraries(
-        ${PROJECT_NAME}
-        PRIVATE
         xxhash
+        magic_enum::magic_enum
 )
