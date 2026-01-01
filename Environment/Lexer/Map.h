67815a51e249700936aa88a1a72f8d4fd1f2425a
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 11:14:25 -0500

    add Map.h to define TokenMap for token type associations

---
+++ Environment/Lexer/Map.h
@@ -0,0 +1,45 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#pragma once
+#include <ankerl/unordered_dense.h>
+#include <celery/string/external.h>
+#include <celery/misc/hash.h>
+#include <celery/misc/string_equal.h>
+
+#include "Token.h"
+
+namespace Typed::Environment::Lexer
+{
+    using TokenMap = ankerl::unordered_dense::map<
+        Celery::Str::External,
+        Token::Type,
+        Celery::Misc::Hash,
+        Celery::Misc::StringEquality
+    >;
+
+    TokenMap Map = {
+        {
+            "=",
+            Token::Type::Equal
+        }
+    };
+}
