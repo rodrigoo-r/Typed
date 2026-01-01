76323372600d4cd94af95733db36658d49a4e941
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 11:13:41 -0500

    add conditional linking for xxhash based on platform in Link.cmake

--- Cmake/Link.cmake
+++ Cmake/Link.cmake
@@ -9,3 +9,13 @@ target_link_libraries(
         Celery::Celery
         unordered_dense::unordered_dense
 )
+
+if (NOT APPLE) # macOS links xxhash statically, so no need to link it again
+    target_link_libraries(
+            ${PROJECT_NAME}
+            PRIVATE
+            xxhash
+    )
+else ()
+    target_link_directories(${PROJECT_NAME} PRIVATE /opt/homebrew/opt/xxhash/lib)
+endif ()
