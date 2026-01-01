3213b4983a5af1686f568cd5d6958bc19dea2230
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 13:01:03 -0500

    add ParseEnv function to populate environment from token stream

---
+++ Environment/Env.cpp
@@ -0,0 +1,60 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#include "Env.h"
+
+#include "Util/Expect.h"
+
+using namespace Typed::Environment;
+
+Env Typed::Environment::ParseEnv(Lexer::TokenStream &stream)
+{
+    Env env;
+
+    // Iterate through the token stream and populate the env map
+    while (stream.HasNext())
+    {
+        Util::Expect(
+            stream,
+            Lexer::Token::Type::Identifier
+        );
+
+        auto key = stream.Curr();
+        Util::Expect(
+            stream,
+            Lexer::Token::Type::Equal
+        );
+        Util::Expect(
+            stream,
+            Lexer::Token::Type::StringLiteral
+        );
+
+        auto value = stream.Curr();
+
+        // Insert into the env map
+        env.emplace(
+            key.value,
+            value.value
+        );
+    }
+
+    return env;
+}
