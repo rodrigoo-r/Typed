be43e56f95b4fd06af48a5ab13b932331410b199
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 12:03:41 -0500

    add Read.cpp and Read.h to implement file reading functionality for the Zelix programming language

---
+++ Environment/Read.h
@@ -0,0 +1,40 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#pragma once
+#include <Celery/String/String.h>
+#include <Celery/Except/CouldNotRead.h>
+#include <Celery/File/Read.h>
+
+namespace Typed::Environment
+{
+    inline Celery::Str::String Read()
+    {
+        auto path = getenv("ZELIX_TYPED_ENV_PATH");
+        if (path == nullptr)
+        {
+            throw Celery::Except::CouldNotRead();
+        }
+
+        auto file = Celery::File::Read(path);
+        return file;
+    }
+}
