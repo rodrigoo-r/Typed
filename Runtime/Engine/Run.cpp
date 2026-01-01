d08eb438546a938d84ae19b27b4c08908c1cafd1
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 18:29:38 -0500

    feat: add Run function to execute main procedure in Engine

---
+++ Runtime/Engine/Run.cpp
@@ -0,0 +1,40 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#include "Run.h"
+
+#include "Rule/Procedure/Procedure.h"
+
+using namespace Typed::Runtime;
+
+void Engine::Run(Symbol::Unit &unit)
+{
+    Celery::Str::External main_str = "Main";
+    auto main_proc_it = unit.Procedures.find(main_str);
+
+    if (main_proc_it == unit.Procedures.end())
+    {
+        // Main procedure not found
+        throw Celery::Except::Exception(main_str.Ptr());
+    }
+
+    Rule::Procedure(main_proc_it->second);
+}
