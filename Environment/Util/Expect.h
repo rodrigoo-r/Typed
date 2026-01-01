c4953a2345141ddbe1c7989d3eb58a940894e14f
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 13:01:13 -0500

    add Expect function to validate token type in Lexer

---
+++ Environment/Util/Expect.h
@@ -0,0 +1,44 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#pragma once
+#include "Environment/Lexer/Except.h"
+#include "Environment/Lexer/Lexer.h"
+
+namespace Typed::Environment::Util
+{
+    inline void Expect(
+        Lexer::TokenStream &stream,
+        Lexer::Token::Type expected_type
+    )
+    {
+        if (!stream.HasNext())
+        {
+            throw Lexer::Exception{stream};
+        }
+
+        auto &token = stream.Next();
+        if (token.type != expected_type)
+        {
+            throw Lexer::Exception{stream};
+        }
+    }
+}
