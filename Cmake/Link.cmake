7b7aca9c5c1832ba8b7e7d5ad15a4a7c08c3d977
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 12:13:32 -0500

    refactor linking logic for macOS to ensure xxhash is linked correctly

--- Cmake/Link.cmake
+++ Cmake/Link.cmake
@@ -10,12 +10,12 @@ target_link_libraries(
         unordered_dense::unordered_dense
 )
 
-if (NOT APPLE) # macOS links xxhash statically, so no need to link it again
-    target_link_libraries(
-            ${PROJECT_NAME}
-            PRIVATE
-            xxhash
-    )
-else ()
+if (APPLE) # macOS links xxhash statically, so no need to link it again
     target_link_directories(${PROJECT_NAME} PRIVATE /opt/homebrew/opt/xxhash/lib)
 endif ()
+
+target_link_libraries(
+        ${PROJECT_NAME}
+        PRIVATE
+        xxhash
+)
