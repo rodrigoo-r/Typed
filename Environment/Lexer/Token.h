473f92eb46649500a218c890ad14fc561bcee3f1
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 10:59:16 -0500

    add Token.h to define the Token structure and its types for the lexer

---
+++ Environment/Lexer/Token.h
@@ -0,0 +1,35 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#pragma once
+
+namespace Typed::Environment::Lexer
+{
+    struct Token
+    {
+        enum class Type
+        {
+            Identifier,
+            Equal,
+            StringLiteral
+        };
+    };
+}
