fc90e89efd9db026f3697eb9b81a39d9510a27f2
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 11:35:12 -0500

    add Lexer.cpp to implement tokenization logic for the Zelix programming language

---
+++ Environment/Lexer/Lexer.cpp
@@ -0,0 +1,104 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#include "Lexer.h"
+
+#include "Environment/Util/FlushToken.h"
+#include "Environment/Util/IsIdentifier.h"
+#include "Except.h"
+#include "State.h"
+
+using namespace Typed::Environment;
+
+Lexer::TokenStream Lexer::Tokenize(
+    Celery::Str::String &source
+)
+{
+    TokenStream stream;
+    State state;
+
+    for (auto i = 0; i < source.Len(); i++)
+    {
+        const auto &ch = source[i];
+        if (state.Len == 0)
+        {
+            // Start new token
+            state.Start = i;
+            state.StringLiteral = false;
+            state.Identifier = false;
+        }
+
+        // Detect identifiers
+        if (state.Len == 0 && Util::IsIdentifier(ch))
+        {
+            state.Identifier = true;
+        }
+
+        // Detect string literals
+        if (ch == '"')
+        {
+            if (state.StringLiteral)
+            {
+                // End of string literal
+                Util::FlushToken(
+                    source,
+                    stream,
+                    state
+                );
+
+                state.StringLiteral = false;
+                continue;
+            }
+
+            state.StringLiteral = true;
+            continue;
+        }
+
+        // Break on punctuation
+        if (ch == '=')
+        {
+            Util::FlushToken(
+                source,
+                stream,
+                state
+            );
+
+            state.Len = 1;
+            Util::FlushToken(
+                source,
+                stream,
+                state
+            );
+
+            continue;
+        }
+
+        // Validate identifier characters
+        if (state.Identifier && !Util::IsIdentifier(ch))
+        {
+            throw Exception(state);
+        }
+
+        state.Len++;
+    }
+
+    return stream;
+}
