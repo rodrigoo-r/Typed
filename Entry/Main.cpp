bb603a0959a0ce729d1031bd881ba1a2f678974f
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 21:26:05 -0500

    feat: update main function to accept command-line arguments and improve error handling

--- Entry/Main.cpp
+++ Entry/Main.cpp
@@ -19,12 +19,41 @@
 // Created by rodrigo on 1/1/26.
 //
 
+#include <Celery/Io/Io.h>
+
+#include "Core/Parser/Parser.h"
+#include "Environment/Env.h"
 #include "Environment/Read.h"
+#include "Runtime/Engine/Run.h"
+#include "Runtime/Unit/Get.h"
+#include "Shared/Lexer/Lexer.h"
 
 using namespace Typed;
 
-int main()
+int main(const int argc, const char **argv)
 {
-    auto env = Environment::Read();
+    if (argc < 2)
+    {
+        Celery::Io::Println("Usage: printed <source.zt>");
+        return 1;
+    }
+
+    // Parse the environment file
+    auto env_file = Environment::Read();
+    auto env_tokens = Typed::Shared::Lexer::Tokenize<
+        Shared::Lexer::LexerType::Environment
+    >(env_file);
+    auto env = Environment::ParseEnv(env_tokens);
+
+    // Parse the source file
+    auto source = Celery::File::Read(argv[1]);
+    auto tokens = Typed::Shared::Lexer::Tokenize<
+        Shared::Lexer::LexerType::Core
+    >(source);
+    const auto ast = Core::Parser::Parse(tokens);
+    auto unit = Runtime::Unit::Get(ast);
+
+    // Run the unit
+    Runtime::Engine::Run(unit, env);
     return 0;
 }
