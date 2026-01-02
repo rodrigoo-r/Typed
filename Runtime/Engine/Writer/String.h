7cf3e7753001601a0352f6ecabd233045b5a8a77
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 20:07:46 -0500

    feat: add String class for handling string writing

---
+++ Runtime/Engine/Writer/String.h
@@ -0,0 +1,40 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 1/1/26.
+//
+
+#pragma once
+#include <Celery/Io/Io.h>
+
+#include "Runtime/Engine/Scope/Writer.h"
+
+namespace Typed::Runtime::Engine::Writer
+{
+    class String :
+        public Scope::Writer
+    {
+    public:
+        Celery::Str::String Handle;
+
+        void Write(Celery::Str::External &str) override
+        {
+            Handle.Write(str.Ptr(), str.Size());
+        }
+    };
+}
