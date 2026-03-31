70599ddd4080bdfb348e8b8ead2a99ebb66734f5
rodrigoo-r <rodrigo@zelixlang.dev>
2026-03-31 23:15:46 +0200

    refactor: improve error reporting format in Cli.Reporter

--- src/cli-reporter.adb
+++ src/cli-reporter.adb
@@ -1,7 +1,9 @@
+with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Exceptions; use Ada.Exceptions;
 with Ada.Text_IO; use Ada.Text_IO;
 with Terminal; use Terminal;
 with Ansi; use Ansi;
+with Wide_Terminal;
 
 package body Cli.Reporter is
 
@@ -9,9 +11,12 @@ package body Cli.Reporter is
       Identity : constant String := Exception_Name    (Ex);
       Msg      : constant String := Exception_Message (Ex);
    begin
-      Write_Colored (Bright_Red & Bold, "error: " & Identity);
-      Put_Line (""); -- Empty line for spacing
+      Write_Line_Colored (Bright_Red & Bold, "error: " & Identity);
+      Wide_Terminal.Write_Colored (Bright_Black, "  ↳ ");
       Put_Line (Msg);
+      Put_Line (""); -- Empty line for spacing
+      Write_Line_Colored (Bright_Black, "(!) Exiting with failure status");
+      Set_Exit_Status (1);
    end Report;
 
 end Cli.Reporter;
