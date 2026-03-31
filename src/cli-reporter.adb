9161afa5f6f10d067ce2118f187e0018f318fa49
rodrigoo-r <rodrigo@zelixlang.dev>
2026-03-31 21:45:27 +0200

    feat: enhance error reporting with colored output in Cli.Reporter

--- src/cli-reporter.adb
+++ src/cli-reporter.adb
@@ -1,5 +1,7 @@
 with Ada.Exceptions; use Ada.Exceptions;
 with Ada.Text_IO; use Ada.Text_IO;
+with Terminal; use Terminal;
+with Ansi; use Ansi;
 
 package body Cli.Reporter is
 
@@ -7,7 +9,7 @@ package body Cli.Reporter is
       Identity : constant String := Exception_Name    (Ex);
       Msg      : constant String := Exception_Message (Ex);
    begin
-      Put_Line ("error: " & Identity);
+      Write_Colored (Bright_Red & Bold, "error: " & Identity);
       Put_Line (""); -- Empty line for spacing
       Put_Line (Msg);
    end Report;
