6a38d97dec01c740d362718f63b80bc62d81b921
rodrigoo-r <rodrigo@zelixlang.dev>
2026-03-31 21:11:08 +0200

    refactor: simplify error reporting in Cli.Reporter by removing unneeded conversions and conditions

--- src/cli-reporter.adb
+++ src/cli-reporter.adb
@@ -1,20 +1,15 @@
 with Ada.Exceptions; use Ada.Exceptions;
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Cli.Exceptions; use Cli.Exceptions;
 
 package body Cli.Reporter is
 
    procedure Report (Ex : Exception_Occurrence) is
-      Msg : Unbounded_String := To_Unbounded_String (Exception_Message (Ex));
+      Identity : constant String := Exception_Name    (Ex);
+      Msg      : constant String := Exception_Message (Ex);
    begin
-      Put ("error: ");
-
-      if Exception_Identity (Ex) = No_Arguments'Identity then
-         Msg := To_Unbounded_String ("No arguments provided.");
-      end if;
-
-      Put_Line (To_String (Msg));
+      Put_Line ("error: " & Identity);
+      Put_Line (""); -- Empty line for spacing
+      Put_Line (Msg);
    end Report;
 
 end Cli.Reporter;
