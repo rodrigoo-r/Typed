a36f3e474c3a3993ccffe49cf47cbc8a6fd8d583
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-15 19:48:05 -0500

    add Libs.cmake for project metadata and licensing information

---
+++ Build/Priority/High/Libs.cmake
@@ -0,0 +1,13 @@
+#-----------------------------------------------------#
+#                                                     #
+#                           Typed                     #
+#                   A text formatting DSL             #
+#                                                     #
+#-----------------------------------------------------#
+#                                                     #
+#         Created by Rodrigo R. & Contributors        #
+#         Released under the Apache License 2.0       #
+#            Check LICENSE.MD for more info           #
+#                                                     #
+#-----------------------------------------------------#
+
