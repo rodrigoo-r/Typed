7279a5d38c599cb5dd0fd292f2743eabf3c7c928
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-16 14:11:09 -0500

    add: create TokenType enum in TokenType.h for text formatting DSL

---
+++ Src/ADT/Lang/TokenType.h
@@ -0,0 +1,49 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/16/26.
+//
+
+#pragma once
+
+namespace Typed::ADT::Lang
+{
+    enum class TokenType
+    {
+        // Import keywords
+        Use,
+
+        // Definitions
+        Procedure,
+
+        // Delimiters
+        Begin,
+        EndProcedure,
+        As,
+        With,
+        Arguments,
+        To,
+
+        // Instructions
+        Declare,
+        Call,
+        Call_Method,
+        Add,
+        Sub,
+        Mul,
+        Div
+    };
+}
