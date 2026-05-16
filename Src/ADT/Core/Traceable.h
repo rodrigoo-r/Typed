68ce6a46a0fcded06432274c07dbb82cdc364682
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-16 14:07:30 -0500

    add: implement Traceable struct in Traceable.h for text formatting DSL

---
+++ Src/ADT/Core/Traceable.h
@@ -0,0 +1,30 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/16/26.
+//
+
+#pragma once
+#include <Celery/Trait/Default.h>
+
+namespace Typed::ADT::Core
+{
+    struct Traceable
+    {
+        Celery::Trait::VeryLarge Line;
+        Celery::Trait::VeryLarge Column;
+    };
+}
