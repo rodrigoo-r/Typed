c6b81981d6926b3089679f31571d1756b1bbd3be
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-16 12:14:53 -0500

    add HelperSetup.cmake to define path for helper files

---
+++ Build/Priority/Highest/HelperSetup.cmake
@@ -0,0 +1,15 @@
+#-----------------------------------------------------#
+#                                                     #
+#                           Typed                     #
+#                   A text formatting DSL             #
+#                                                     #
+#-----------------------------------------------------#
+#                                                     #
+#         Created by Rodrigo R. & Contributors        #
+#         Released under the Apache License 2.0       #
+#            Check LICENSE.MD for more info           #
+#                                                     #
+#-----------------------------------------------------#
+
+# Set the path for the helper files
+set(Helper_Dir "${Build_Abs_Path}/Helper")
