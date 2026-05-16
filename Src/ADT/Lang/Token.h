5e2b103d0616877782cf85d5cb5a9321e66097e9
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-16 14:11:12 -0500

    add: create Token struct in Token.h for text formatting DSL

---
+++ Src/ADT/Lang/Token.h
@@ -0,0 +1,34 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/16/26.
+//
+
+#pragma once
+#include <Celery/String/External.h>
+
+#include "ADT/Core/Traceable.h"
+#include "TokenType.h"
+
+namespace Typed::ADT::Lang
+{
+    struct Token :
+        Core::Traceable
+    {
+        Celery::Str::External Value;
+        TokenType type;
+    };
+}
