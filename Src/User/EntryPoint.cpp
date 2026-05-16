1d389dae75d20552a2b87c144ee69f54ec000e8a
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-15 19:36:50 -0500

    add EntryPoint.cpp with initial main function

---
+++ Src/User/EntryPoint.cpp
@@ -0,0 +1,23 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/15/26.
+//
+
+int main()
+{
+    return 0;
+}
