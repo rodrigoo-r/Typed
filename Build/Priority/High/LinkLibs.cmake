1169f94afe0d1a3fe3769bd39081b2b37da779ef
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-15 19:58:26 -0500

    add LinkLibs.cmake to link Celery and CLI11 libraries

---
+++ Build/Priority/High/LinkLibs.cmake
@@ -0,0 +1,21 @@
+#-----------------------------------------------------#
+#                                                     #
+#                           Typed                     #
+#                   A text formatting DSL             #
+#                                                     #
+#-----------------------------------------------------#
+#                                                     #
+#         Created by Rodrigo R. & Contributors        #
+#         Released under the Apache License 2.0       #
+#            Check LICENSE.MD for more info           #
+#                                                     #
+#-----------------------------------------------------#
+
+# Link all libraries
+target_link_libraries(
+        ${PROJECT_NAME}
+        PRIVATE
+        Celery::Celery
+        CLI11::CLI11
+        #unordered_dense::unordered_dense
+)
