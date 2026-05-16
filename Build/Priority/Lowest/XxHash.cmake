cc5926cfd29fa218e78d0ca9c4d1d428b6264791
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-15 19:56:47 -0500

    add XxHash.cmake to configure xxHash library linking and include directories

---
+++ Build/Priority/Lowest/XxHash.cmake
@@ -0,0 +1,45 @@
+#-----------------------------------------------------#
+#                                                     #
+#                           Typed                     #
+#                   A text formatting DSL             #
+#                                                     #
+#-----------------------------------------------------#
+#                                                     #
+#         Created by Rodrigo R. & Contributors        #
+#         Released under the Apache License 2.0       #
+#            Check LICENSE.MD for more info           #
+#                                                     #
+#-----------------------------------------------------#
+
+
+if (APPLE AND UNIX)
+    target_link_directories(
+            ${PROJECT_NAME}
+            PRIVATE
+            /opt/homebrew/opt/xxhash/lib
+    )
+
+    target_include_directories(
+            ${PROJECT_NAME}
+            PRIVATE
+            /opt/homebrew/opt/xxhash/include
+    )
+else ()
+    target_include_directories(
+            xxhash
+            PUBLIC
+            ${xxhash_SOURCE_DIR}
+    )
+
+    add_library(
+            xxhash
+            STATIC
+            ${xxhash_SOURCE_DIR}/xxhash.c
+    )
+
+    target_link_libraries(
+            ${PROJECT_NAME}
+            PRIVATE
+            xxhash
+    )
+endif ()
