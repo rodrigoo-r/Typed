6f6065079c6a4bb861ef3963701a84314c1becb4
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-16 13:14:28 -0500

    add Handler files for subcommand execution

---
+++ Src/Terminal/Subcommand/Run/Handler.cpp
@@ -0,0 +1,30 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/16/26.
+//
+
+#include "Handler.h"
+
+#include <Celery/Io/Io.h>
+
+using namespace Typed;
+using namespace Typed::Terminal;
+
+void Subcommand::Run(const std::string &input)
+{
+
+}
---
+++ Src/Terminal/Subcommand/Run/Handler.h
@@ -0,0 +1,26 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/16/26.
+//
+
+#pragma once
+#include <string>
+
+namespace Typed::Terminal::Subcommand
+{
+    void Run(const std::string &);
+}
