c53c24b7f3663cd58127df1d4ce1249f32751a2f
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-16 15:24:02 -0500

    add: create File alias in File.h for Typed DSL

---
+++ Src/ADT/Stream/File.h
@@ -0,0 +1,28 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/16/26.
+//
+
+#pragma once
+
+#include <Celery/Array/Stream.h>
+
+namespace Typed::ADT::Stream
+{
+    using File =
+        Celery::Array::Stream<char>;
+}
