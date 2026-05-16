23e576d06abc73d83bde671f88e366f980384b54
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-15 19:32:12 -0500

    add priority-based CMake file inclusion in Delegate.cmake

--- Build/Delegate.cmake
+++ Build/Delegate.cmake
@@ -11,4 +11,43 @@
 #                                                     #
 #-----------------------------------------------------#
 
-include(Source.cmake)
+# Defines the path for the Build directory, which
+# contains the CMake files for the project.
+set(BUILD_ABS_PATH "${CMAKE_SOURCE_DIR}/Build")
+set(BUILD_FILES_PRIORITY_PATH "${BUILD_ABS_PATH}/Priority")
+
+# Now, we include the CMake files by priority
+# In the order:
+# - Highest
+# - High
+# - Normal
+# - Low
+# - Lowest
+set(
+    PRIORITY_FILES_ORDER
+    "Highest"
+    "High"
+    "Normal"
+    "Low"
+    "Lowest"
+)
+
+# Loop through the priority files and include them
+foreach (PRIORITY ${PRIORITY_FILES_ORDER})
+    set(PRIORITY_PATH "${BUILD_FILES_PRIORITY_PATH}/${PRIORITY}")
+    if (EXISTS ${PRIORITY_PATH})
+        # Make sure the path is a directory
+        if (NOT IS_DIRECTORY ${PRIORITY_PATH})
+            message(
+                    FATAL_ERROR
+                    "The path ${PRIORITY_PATH} is not a directory."
+            )
+        endif ()
+
+        # Loop through the CMake files in the priority directory and include them
+        file(GLOB PRIORITY_FILES "${PRIORITY_PATH}/*.cmake")
+        foreach (PRIORITY_FILE ${PRIORITY_FILES})
+            include(${PRIORITY_FILE})
+        endforeach ()
+    endif ()
+endforeach ()
