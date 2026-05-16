2702f471931be95d32212a248e0a3ce6ffe87cea
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-15 19:53:58 -0500

    add FetchContent declarations for CLI11, Celery, unordered_dense, and conditional xxHash

--- Build/Priority/High/Libs.cmake
+++ Build/Priority/High/Libs.cmake
@@ -11,3 +11,39 @@
 #                                                     #
 #-----------------------------------------------------#
 
+include(FetchContent)
+
+# Fetch CLI11
+FetchContent_Declare(
+        CLI11
+        GIT_REPOSITORY https://github.com/CLIUtils/CLI11
+        GIT_TAG v2.6.2
+)
+
+# Fetch Celery
+FetchContent_Declare(
+        Celery
+        GIT_REPOSITORY https://github.com/rodrigoo-r/Celery.git
+        GIT_TAG master
+)
+
+FetchContent_Declare(
+        unordered_dense
+        GIT_REPOSITORY https://github.com/martinus/unordered_dense
+        GIT_TAG        v4.8.1
+)
+
+# Fetch xxHash only if the platform is not Apple
+# For some weird reason, xxHash does not compile on macOS, so it's
+# better for macOS users to install it system-wide instead of using the bundled version
+if (NOT APPLE)
+    FetchContent_Declare(
+            xxhash
+            GIT_REPOSITORY https://github.com/Cyan4973/xxHash.git
+            GIT_TAG        v0.8.3
+    )
+
+    FetchContent_MakeAvailable(xxhash)
+endif ()
+
+FetchContent_MakeAvailable(CLI11 Celery unordered_dense)
