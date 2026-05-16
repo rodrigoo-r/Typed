121527728ec55ea41ca1d1a859a345d957373bdc
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-16 12:06:07 -0500

    add BlackMagicSetup.cmake to define global variable for BlackMagic directory

---
+++ Build/Priority/High/BlackMagicSetup.cmake
@@ -0,0 +1,15 @@
+#-----------------------------------------------------#
+#                                                     #
+#                           Typed                     #
+#                   A text formatting DSL             #
+#                                                     #
+#-----------------------------------------------------#
+#                                                     #
+#         Created by Rodrigo R. & Contributors        #
+#         Released under the Apache License 2.0       #
+#            Check LICENSE.MD for more info           #
+#                                                     #
+#-----------------------------------------------------#
+
+# Set a global variable to point to the BlackMagic dir
+set(BLACKMAGIC_DIR "${CMAKE_SOURCE_DIR}/BlackMagic")
