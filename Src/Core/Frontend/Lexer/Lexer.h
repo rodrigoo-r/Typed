68f0e42891e416de2845e9c11910ea76bfb838fc
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-17 17:43:53 -0500

    add: create Lexer class in Lexer.h for Typed DSL

---
+++ Src/Core/Frontend/Lexer/Lexer.h
@@ -0,0 +1,50 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/16/26.
+//
+
+#pragma once
+#include "ADT/Stream/Token.h"
+
+namespace Typed::Core::Frontend::Lexer
+{
+    class Machine
+    {
+    protected:
+        ADT::Stream::Token Tokens;
+
+        void Comment();
+
+    public:
+        using EntryRef =
+            Celery::Array::Stream<char> &;
+
+        using ConstStreamRef =
+            const ADT::Stream::Token&;
+
+    private:
+
+        EntryRef Contents;
+
+    public:
+        Machine(EntryRef contents) :
+            Contents(contents)
+        {}
+
+        ConstStreamRef Lex(const std::string &input);
+    };
+}
