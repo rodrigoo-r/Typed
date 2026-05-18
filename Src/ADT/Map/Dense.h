94e9d4a9241de83458315ee52c482cadbc1cf341
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 12:42:59 -0500

    feat: add Dense template alias for unordered dense map in Typed::ADT::Map

---
+++ Src/ADT/Map/Dense.h
@@ -0,0 +1,28 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#pragma once
+#include <ankerl/unordered_dense.h>
+
+namespace Typed::ADT::Map
+{
+    template <typename Key, typename Value>
+    using Dense =
+        ankerl::unordered_dense::map<Key, Value>;
+}
