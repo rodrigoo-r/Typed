21d1b1942163d1a964f50ca0b3fe7f05932845b5
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 13:07:08 -0500

    feat: implement Flush method for token processing in Machine class

---
+++ Src/Core/Frontend/Lexer/Flush.cpp
@@ -0,0 +1,81 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#include "ADT/Exception/UnknownToken.h"
+#include "Lexer.h"
+#include "Support/Lang/TokenMap.h"
+
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Lexer;
+
+void Machine::Flush()
+{
+    auto start = state.GetStart();
+    auto end = state.GetEnd();
+    auto size = end - start;
+    auto ptr = contents.Ptr() + start;
+    ADT::Lang::Token token{
+        {
+            state.GetLine(),
+            state.GetColumn()
+        },
+        Celery::Str::External{ptr, size},
+        ADT::Lang::TokenType::Add
+    };
+
+    if (size == 0 && !state.IsStringLiteral())
+    {
+        state.Reset();
+        return;
+    }
+
+    if (state.IsStringLiteral())
+    {
+        token.type = ADT::Lang::TokenType::StringLiteral;
+    } else if (state.IsFloatLiteral())
+    {
+        token.type = ADT::Lang::TokenType::NumberLiteral;
+    } else if (state.IsNumberLiteral())
+    {
+        token.type = ADT::Lang::TokenType::NumberLiteral;
+    } else if (state.IsIdentifier())
+    {
+        // Find the token type and assign the appropriate type
+        auto it = Support::Lang::TokenMap.find(token.value);
+        if (it != Support::Lang::TokenMap.end())
+        {
+            token.type = it->second;
+        }
+        else
+        {
+            token.type = ADT::Lang::TokenType::Identifier;
+        }
+    } else
+    {
+        // Unknown token, throw an exception
+        throw ADT::Exception::UnknwonToken(
+            state.GetLine(),
+            state.GetColumn()
+        );
+    }
+
+    tokens.PushBack(std::move(token));
+    state.Reset();
+}
