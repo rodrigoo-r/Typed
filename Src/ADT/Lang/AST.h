0ea7be12540946b9356cc801c15e53b7868acf75
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 17:23:40 -0500

    feat: add AST and ASTType structures for typed DSL representation

---
+++ Src/ADT/Lang/AST.h
@@ -0,0 +1,37 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#pragma once
+#include <Celery/Array/Vector.h>
+#include <Celery/String/External.h>
+
+
+#include "ADT/Core/Traceable.h"
+#include "ASTType.h"
+
+namespace Typed::ADT::Lang
+{
+    struct AST :
+        Core::Traceable
+    {
+        Celery::Str::External value;
+        ASTType type;
+        Celery::Array::Vector<AST *> children;
+    };
+}
---
+++ Src/ADT/Lang/ASTType.h
@@ -0,0 +1,64 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#pragma once
+
+namespace Typed::ADT::Lang
+{
+    enum class ASTType
+    {
+        // Root
+        Root,
+
+        // Top-level declarations
+        Use,
+        Procedure,
+        Arguments,
+        Body,
+
+        // Data Types
+        String,
+        Integer,
+        Float,
+        Boolean,
+
+        // Expression
+        Expressions,
+
+        // Instructions
+        Call,
+        CallMethod,
+        Add,
+        Suv,
+        Div,
+        Mul,
+
+        // Declarations
+        Declare,
+
+        // Literals
+        StringLiteral,
+        NumberLiteral,
+        FloatLiteral,
+        BooleanLiteral,
+
+        // Identifier
+        Identifier
+    };
+}
