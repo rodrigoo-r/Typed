3b92f128e04ff6d916d7357943ec8fa3e64180c2
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 22:09:28 -0500

    feat: implement subcommand execution with file reading and AST printing

--- Src/Terminal/Subcommand/Run/Handler.cpp
+++ Src/Terminal/Subcommand/Run/Handler.cpp
@@ -20,11 +20,28 @@
 #include "Handler.h"
 
 #include <Celery/Io/Io.h>
+#include <magic_enum/magic_enum.hpp>
+
+#include "Core/Frontend/Lexer/Lexer.h"
+#include "Core/Frontend/Parser/Parser.h"
+#include "Support/File/Reader.h"
+#include "Support/Printer/ASTPrinter.h"
 
 using namespace Typed;
 using namespace Typed::Terminal;
 
 void Subcommand::Run(const std::string &input)
 {
+    Support::File::Reader reader;
+    reader.SetPath({input.data(), input.size()});
+
+    auto contents = reader.Read();
+    Core::Frontend::Lexer::Machine lexer(contents);
+    auto &tokens = lexer.Lex();
+
+    Core::Frontend::Parser::Machine parser(tokens);
+    auto root = parser.Parse();
+    Support::Printer::ASTPrinter printer(root);
+    printer.Print();
 
 }
