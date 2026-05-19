3928fd78bc53c7bd882f088c40ef9803d356aa37
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-19 12:43:12 -0500

    feat: add Procedure structure for managing AST and arguments in Typed DSL

---
+++ Src/ADT/PreWalker/Procedure.h
@@ -0,0 +1,42 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/19/26.
+//
+
+#pragma once
+#include "ADT/Runtime/ObjectType.h"
+
+namespace Typed::ADT::PreWalker
+{
+    struct Procedure
+    {
+        using Tree =
+            Lang::AST;
+
+        using TreePtr =
+            Tree *;
+
+        using ArgumentMap =
+            Map::Dense<
+                Celery::Str::External,
+                Runtime::ObjectType
+            >;
+
+        ArgumentMap arguments;
+        TreePtr body;
+    };
+}
