636b14e86a1c819e15f8e18e45904bc6ed9a03fc
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-19 09:58:55 -0500

    feat: add Context structure for managing bytecode context in Typed DSL

---
+++ Src/ADT/Bytecode/Context.h
@@ -0,0 +1,33 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/19/26.
+//
+
+#pragma once
+#include <Celery/Trait/Default.h>
+
+
+#include "ContextType.h"
+
+namespace Typed::ADT::Bytecode
+{
+    struct Context
+    {
+        ContextType type;
+        Celery::Trait::VeryLarge data;
+    };
+}
