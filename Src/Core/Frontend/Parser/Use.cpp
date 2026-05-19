2a831b3f25c7a8c216e5f0ef3965dbf36840a67f
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 22:21:35 -0500

    feat: add token handling in Use function to allocate AST node

--- Src/Core/Frontend/Parser/Use.cpp
+++ Src/Core/Frontend/Parser/Use.cpp
@@ -28,4 +28,12 @@ void Machine::Use()
 {
     // We assume the Use token has been already consumed
     Expect(ADT::Lang::TokenType::Identifier);
+
+    auto &token = tokens.Next();
+
+    // The token's value will be put to the AST, so we
+    // don't have to nest an Identifier AST under the Use AST
+    auto node = AllocateBase(token, ADT::Lang::ASTType::Use);
+
+    root->children.PushBack(node);
 }
