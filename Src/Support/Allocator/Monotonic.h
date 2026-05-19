42a0c5adeca48dd2717e26ed90fa3fbf1d1d1681
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 21:47:32 -0500

    feat: add Monotonic allocator wrapper for typed DSL support

---
+++ Src/Support/Allocator/Monotonic.h
@@ -0,0 +1,28 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#pragma once
+#include <Celery/Memory/Monotonic.h>
+
+namespace Typed::Support::Allocator
+{
+    template <typename Value>
+    using Monotonic =
+        Celery::Pmr::BumpAllocator<Value>;
+}
