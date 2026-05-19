bb7188ca3e64a57c60c42cd155adb8498967367d
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 21:55:39 -0500

    feat: add Parser class for Typed DSL with memory management integration

---
+++ Src/Core/Frontend/Parser/Parser.h
@@ -0,0 +1,58 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#pragma once
+#include "ADT/Lang/AST.h"
+#include "ADT/Stream/Token.h"
+#include "Core/Frontend/Lexer/Lexer.h"
+#include "Support/Allocator/Monotonic.h"
+
+namespace Typed::Core::Frontend::Parser
+{
+    class Machine
+    {
+    protected:
+        using Allocator =
+            Support::Allocator::Monotonic<
+                ADT::Lang::AST
+            >;
+
+        using Tree =
+            ADT::Lang::AST;
+
+        using TreePtr =
+            Tree *;
+
+        TreePtr AllocateBase(
+            ADT::Lang::Token &token,
+            ADT::Lang::ASTType type
+        );
+
+        TreePtr Allocate(ADT::Lang::ASTType type);
+
+        Lexer::Machine::ConstStreamRef tokens;
+
+    public:
+        Machine(Lexer::Machine::ConstStreamRef tokens) :
+            tokens(tokens)
+        {}
+
+        TreePtr Parse();
+    };
+}
