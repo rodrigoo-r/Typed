13d8e2255b45f2f6180cc686d73db9e1939f9250
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 21:59:13 -0500

    feat: add Allocate function for memory allocation in Typed DSL

---
+++ Src/Core/Frontend/Parser/Allocate.cpp
@@ -0,0 +1,32 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#include "Parser.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Parser;
+
+Machine::TreePtr Machine::Allocate(
+    ADT::Lang::ASTType type
+)
+{
+    return AllocateBase(tokens.Next(), type);
+}
