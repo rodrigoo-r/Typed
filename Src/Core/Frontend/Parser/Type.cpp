5eb2120f9b73bd498cd1d0143058d5f0251501a2
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 23:00:33 -0500

    feat: implement type parsing in Type.cpp

---
+++ Src/Core/Frontend/Parser/Type.cpp
@@ -0,0 +1,63 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#include "ADT/Exception/UnexpectedToken.h"
+#include "Parser.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Parser;
+
+Machine::TreePtr Machine::Type()
+{
+    // Get the next token and ensure it's a type
+    auto &token = tokens.Next();
+
+    switch (token.type)
+    {
+        case ADT::Lang::TokenType::String:
+        {
+            return AllocateBase(token, ADT::Lang::ASTType::String);
+        }
+
+        case ADT::Lang::TokenType::Integer:
+        {
+            return AllocateBase(token, ADT::Lang::ASTType::Integer);
+        }
+
+        case ADT::Lang::TokenType::Float:
+        {
+            return AllocateBase(token, ADT::Lang::ASTType::Float);
+        }
+
+        case ADT::Lang::TokenType::Boolean:
+        {
+            return AllocateBase(token, ADT::Lang::ASTType::Boolean);
+        }
+
+        default:
+        {
+            throw ADT::Exception::UnexpectedToken(
+                token.line,
+                token.column
+            );
+        }
+    }
+}
