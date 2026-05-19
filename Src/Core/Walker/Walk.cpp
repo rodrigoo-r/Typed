25816a9499117761d8f97c91057d772c77c44078
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-19 09:32:12 -0500

    feat: implement Walk method for AST traversal in Machine class

---
+++ Src/Core/Walker/Walk.cpp
@@ -0,0 +1,45 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/19/26.
+//
+
+#include "Walker.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Walker;
+
+void Machine::Walk()
+{
+    for (auto &child : core->children)
+    {
+        switch (child->type)
+        {
+            case ADT::Lang::ASTType::Use:
+            {
+                break;
+            }
+
+            case ADT::Lang::ASTType::Procedure:
+            {
+                break;
+            }
+
+            default: break;
+        }
+    }
+}
