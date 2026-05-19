60824344ffd5ec41e937969a36c3fdda87224417
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-19 09:58:51 -0500

    feat: add ContextType enum for managing bytecode context in Typed DSL

---
+++ Src/ADT/Bytecode/ContextType.h
@@ -0,0 +1,31 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/19/26.
+//
+
+#pragma once
+
+namespace Typed::ADT::Bytecode
+{
+    enum class ContextType
+    {
+        // Push/Pull from:
+        Procedure,
+        Stack,
+        String
+    };
+}
