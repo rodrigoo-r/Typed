3c1fc5e27a49a693cc9776154441f5cbc720688f
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 22:09:14 -0500

    feat: add Parse function for processing tokens in Typed DSL

---
+++ Src/Core/Frontend/Parser/Parse.cpp
@@ -0,0 +1,50 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#include "Parser.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Parser;
+
+Machine::TreePtr Machine::Parse()
+{
+    auto root = AllocateBase(tokens.Peek(), ADT::Lang::ASTType::Root);
+
+    while (tokens.HasNext())
+    {
+        auto &token = tokens.Next();
+
+        switch (token.type)
+        {
+            case ADT::Lang::TokenType::Use:
+            {
+                break;
+            }
+
+            default:
+            {
+                break;
+            }
+        }
+    }
+
+    return root;
+}
