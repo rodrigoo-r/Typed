9348aa962507f91964f45c20ac92e42a7b883467
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 22:48:57 -0500

    feat: add UnexpectedToken exception class for handling unknown tokens

---
+++ Src/ADT/Exception/UnexpectedToken.h
@@ -0,0 +1,40 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#pragma once
+#include <Celery/Except/Base.h>
+
+#include "ADT/Core/Traceable.h"
+
+namespace Typed::ADT::Exception
+{
+    class UnexpectedToken :
+        public Celery::Except::Exception,
+        public Core::Traceable
+    {
+    public:
+        UnexpectedToken(
+            Celery::Trait::VeryLarge line,
+            Celery::Trait::VeryLarge column
+        ) :
+            Exception("Unknown token"),
+            Traceable{line, column}
+        {}
+    };
+}
