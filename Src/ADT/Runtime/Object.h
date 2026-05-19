6640d5d96ed4450c11d49fd7b14602d839904cc3
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-19 12:37:01 -0500

    feat: add Object and ObjectType structures for runtime value handling

---
+++ Src/ADT/Runtime/Object.h
@@ -0,0 +1,43 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/19/26.
+//
+
+#pragma once
+#include <Celery/String/External.h>
+
+
+#include "ObjectType.h"
+
+namespace Typed::ADT::Runtime
+{
+    struct Object
+    {
+        // A value can hold either of the 4
+        // primitives allowed
+        using Value =
+            std::variant<
+                int,
+                float,
+                bool,
+                Celery::Str::External
+            >;
+
+        ObjectType type;
+        Value value;
+    };
+}
