b001b07aacc54c2a21a9f25a0039835dca93c050
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 23:17:44 -0500

    feat: implement Declare method in Machine class for syntax declaration

---
+++ Src/Core/Frontend/Parser/Declare.cpp
@@ -0,0 +1,45 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#include "Parser.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Parser;
+
+void Machine::Declare(TreePtr body)
+{
+    // Syntax:
+    // Declare <Identifier> As <Type> With <Expression>
+    auto decl = Allocate(ADT::Lang::ASTType::Declare);    // Consume the Declare token
+    body->children.PushBack(decl);
+
+    auto &name = tokens.Peek();
+    Expect(ADT::Lang::TokenType::Identifier);
+    Expect(ADT::Lang::TokenType::As);
+
+    auto type = Type();
+    Expect(ADT::Lang::TokenType::With);
+
+    // Build the AST structure
+    decl->children.PushBack(AllocateBase(name, ADT::Lang::ASTType::Identifier));
+    decl->children.PushBack(type);
+    Expression(decl);
+}
