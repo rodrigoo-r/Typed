0728b8f23245b09ce18ce7afc73e5d9e16f85233
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-18 21:57:49 -0500

    feat: implement AllocateBase function for memory allocation in Typed DSL

---
+++ Src/Core/Frontend/Parser/AllocateBase.cpp
@@ -0,0 +1,40 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#include "Parser.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Parser;
+
+Machine::TreePtr Machine::AllocateBase(
+    ADT::Lang::Token &token,
+    ADT::Lang::ASTType type
+)
+{
+    return Allocator::Allocate(
+        ADT::Core::Traceable{
+            token.line,
+            token.column
+        },
+        type,
+        token.value
+    );
+}
