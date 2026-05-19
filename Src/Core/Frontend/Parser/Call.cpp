8bac3f1babc4a144f360640bf032067dcf5581f0
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-19 00:57:18 -0500

    feat: add Call function for parsing function calls in the AST

---
+++ Src/Core/Frontend/Parser/Call.cpp
@@ -0,0 +1,58 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/19/26.
+//
+
+#include "ADT/Exception/UnexpectedToken.h"
+#include "Parser.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Parser;
+
+Machine::TreePtr Machine::Call(TokenStreamView &input)
+{
+    auto &name = input.Peek();
+    Expect(input, ADT::Lang::TokenType::Identifier);
+
+    // Allocate the call node
+    auto call = AllocateBase(name, ADT::Lang::ASTType::Call);
+    call->children.PushBack(AllocateBase(name, ADT::Lang::ASTType::Identifier));
+
+    // Parse arguments if needed
+    if (input.HasNext())
+    {
+        auto &peek = input.Peek();
+        if (peek.type != ADT::Lang::TokenType::With)
+        {
+            throw ADT::Exception::UnexpectedToken(
+                peek.line,
+                peek.column
+            );
+        }
+
+        // Consume the token and parse arguments
+        tokens.Next();
+
+        auto args = AllocateBase(peek, ADT::Lang::ASTType::Arguments);
+        call->children.PushBack(args);
+        args->children.PushBack(CallArgs(input));
+    }
+
+    return call;
+}
