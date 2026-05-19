40b9b049b5a1f24643d897b3fc2a0ac9ea950549
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-19 01:26:14 -0500

    feat: implement Add function for parsing addition expressions

---
+++ Src/Core/Frontend/Parser/Add.cpp
@@ -0,0 +1,33 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/19/26.
+//
+
+#include "Parser.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Parser;
+
+Machine::TreePtr Machine::Add(TokenStreamView &input)
+{
+    auto &num = input.Next();
+
+    // Validate that the input is a number
+
+}
