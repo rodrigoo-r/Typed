03605fe7a9552b40bd01b5a0d7f4748445fc90c5
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 07:09:46 -0500

    feat: add Walk class for executing the main procedure in Typed DSL

---
+++ Src/Core/Backend/Walk.cpp
@@ -0,0 +1,36 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/20/26.
+//
+
+#include "ADT/Exception/UnknownEntryPoint.h"
+#include "Walker.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Backend;
+
+void Walker::Walk()
+{
+    // Get the main procedure
+    auto main = runnable.procedures.find("Main");
+    if (main == runnable.procedures.end())
+        throw ADT::Exception::UnknownEntryPoint();
+
+    // Run the main procedure
+    Procedure(main->second);
+}
