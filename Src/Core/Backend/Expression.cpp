439db92052f09ccf8ed0dd083003e87b634912c9
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 08:23:08 -0500

    feat: extend Expression walker to handle various AST types

--- Src/Core/Backend/Expression.cpp
+++ Src/Core/Backend/Expression.cpp
@@ -23,10 +23,77 @@ using namespace Typed;
 using namespace Typed::Core;
 using namespace Typed::Core::Backend;
 
-void Walker::Expression(
+ADT::Runtime::Object Walker::Expression(
     VariableMap &stack,
     TreePtr body
 )
 {
+    auto &expr = body->children[0];
 
+    switch (expr->type)
+    {
+        case ADT::Lang::ASTType::Expression:
+        {
+            return Expression(stack, expr);
+        }
+
+        case ADT::Lang::ASTType::Call:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::Add:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::Sub:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::Mul:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::Div:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::StringLiteral:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::FalseLiteral:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::TrueLiteral:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::FloatLiteral:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::NumberLiteral:
+        {
+            break;
+        }
+
+        case ADT::Lang::ASTType::Identifier:
+        {
+            break;
+        }
+
+        default: break;
+    }
+
+    return {};
 }
