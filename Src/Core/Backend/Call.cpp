41d8ff9ca7b9690fa2b3cbaa8dc50dd888fcc708
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 09:42:56 -0500

    feat: implement Call function in Walker for procedure invocation

---
+++ Src/Core/Backend/Call.cpp
@@ -0,0 +1,61 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/20/26.
+//
+
+#include "ADT/Exception/UnknownProcedure.h"
+#include "Walker.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Backend;
+
+ADT::Runtime::Object Walker::Call(
+    VariableMap &stack,
+    TreePtr body
+)
+{
+    auto &children = body->children;
+    auto &name = children[0]->value;
+    auto line = body->line;
+    auto column = body->column;
+    auto proc = runnable.procedures.find(name);
+
+    if (proc == runnable.procedures.end())
+    {
+        throw ADT::Exception::UnknownProcedure(line, column);
+    }
+
+    if (children.Size() == 1)
+    {
+        ArgumentList args;
+        Procedure(proc->second, args, body);
+    }
+
+    // Get args too
+    ArgumentList args;
+    auto args_node = children[1];
+
+    for (auto &arg_node : args_node->children)
+    {
+        auto arg = arg_node->children[0];
+        args.PushBack(Expression(stack, arg));
+    }
+
+    Procedure(proc->second, args, body);
+    return {};
+}
