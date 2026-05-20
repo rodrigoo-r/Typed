62089bdafc0b0100ad6bd1636509be0a616e930f
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 14:50:12 -0500

    feat: add TypeCheck function for runtime type validation

---
+++ Src/Support/Runtime/TypeChecker.h
@@ -0,0 +1,30 @@
+//
+// Created by Rodrigo on 5/20/26.
+//
+
+#pragma once
+#include "ADT/Exception/MismatchedType.h"
+#include "ADT/Runtime/Object.h"
+
+namespace Typed::Support::Runtime
+{
+    inline void TypeCheck(
+        ADT::Runtime::ObjectType expected,
+        ADT::Runtime::ObjectType actual,
+        Celery::Trait::VeryLarge line,
+        Celery::Trait::VeryLarge column
+    )
+        {
+            // Find the expected type
+            if (
+                expected != actual &&
+                expected != ADT::Runtime::ObjectType::Any
+            )
+            {
+                throw ADT::Exception::MismatchedType(
+                    line,
+                    column
+                );
+            }
+        }
+}
