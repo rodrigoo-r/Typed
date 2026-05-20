5f3bf3b8976970a4292588b7b92e8663172c4a8f
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 14:57:31 -0500

    feat: add GetObjValue and type-specific getters for runtime object access

---
+++ Src/Support/Runtime/GetObjValue.h
@@ -0,0 +1,53 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+
+//
+// Created by Rodrigo on 5/20/26.
+//
+
+#pragma once
+#include <Celery/String/External.h>
+
+#include "ADT/Runtime/Object.h"
+
+namespace Typed::Support::Runtime
+{
+    template <typename T>
+    T &GetObjValue(ADT::Runtime::Object &obj)
+    {
+        return std::get<T>(obj.value);
+    }
+
+    inline Celery::Str::External &GetStrObj(ADT::Runtime::Object &obj)
+    {
+        return GetObjValue<Celery::Str::External>(obj);
+    }
+
+    inline float &GetFloatObj(ADT::Runtime::Object &obj)
+    {
+        return GetObjValue<float>(obj);
+    }
+
+    inline int &GetIntObj(ADT::Runtime::Object &obj)
+    {
+        return GetObjValue<int>(obj);
+    }
+
+    inline bool &GetBoolObj(ADT::Runtime::Object &obj)
+    {
+        return GetObjValue<bool>(obj);
+    }
+}
