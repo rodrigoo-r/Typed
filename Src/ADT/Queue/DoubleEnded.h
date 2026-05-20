9ce7a0b092d157e9f2ecb401802f176f4e52af88
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 08:05:12 -0500

    feat: add DoubleEnded type alias using std::deque for queue implementation

---
+++ Src/ADT/Queue/DoubleEnded.h
@@ -0,0 +1,28 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/20/26.
+//
+
+#pragma once
+#include <deque>
+
+namespace Typed::ADT::Queue
+{
+    template <typename T>
+    using DoubleEnded =
+        std::deque<T>;
+}
