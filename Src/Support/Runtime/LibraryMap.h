8439cf52b1bff043467a75d20953dad6fe4ea7a1
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 10:32:02 -0500

    feat: add LibraryMap.h for managing IO package procedures

---
+++ Src/Support/Runtime/LibraryMap.h
@@ -0,0 +1,38 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/20/26.
+//
+
+#pragma once
+#include <Celery/Misc/Hash.h>
+#include <Celery/String/External.h>
+
+
+#include "ADT/List/Procedure.h"
+#include "ADT/Map/Dense.h"
+#include "Runtime/IO/Package.h"
+
+namespace Typed::Support::Runtime
+{
+    inline ADT::Map::Dense<
+        Celery::Str::External,
+        ADT::List::Procedure,
+        Celery::Misc::Hash
+    > LibraryMap = {
+        {"IO", Typed::Runtime::IO::Package}
+    };
+}
