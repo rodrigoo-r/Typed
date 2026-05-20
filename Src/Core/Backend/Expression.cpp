3e6d6187cdf6dd6be1d3a0d1dad8a3086cd473e8
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 07:34:01 -0500

    feat: add Expression method to Walker class for handling expressions

---
+++ Src/Core/Backend/Expression.cpp
@@ -0,0 +1,32 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/20/26.
+//
+
+#include "Walker.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Backend;
+
+void Walker::Expression(
+    VariableMap &maps,
+    TreePtr body
+)
+{
+
+}
