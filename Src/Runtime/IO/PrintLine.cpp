83d95080ef63c4520e153b807f261d3a065bb2be
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-20 16:35:50 -0500

    feat: add PrintLine function for enhanced text output

---
+++ Src/Runtime/IO/PrintLine.cpp
@@ -0,0 +1,33 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/20/26.
+//
+
+#include "PrintLine.h"
+
+#include "Print.h"
+#include "Support/Printer/ASTPrinter.h"
+
+using namespace Typed;
+using namespace Typed::Runtime;
+using namespace Typed::Runtime::IO;
+
+void IO::PrintLine(ADT::List::Object &args, ADT::Lang::AST *trace)
+{
+    Print(args, trace);
+    Celery::Io::Println();
+}
