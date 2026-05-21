fb39e1a35c310c6d5311858d1ccd562c6c94eedf
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-21 18:26:00 -0500

    feat: handle unexpected returns in Return statement handler

--- Src/Core/Backend/Return.cpp
+++ Src/Core/Backend/Return.cpp
@@ -17,6 +17,7 @@
 // Created by Rodrigo on 5/21/26.
 //
 
+#include "ADT/Exception/UnexpectedReturn.h"
 #include "Walker.h"
 
 using namespace Typed;
@@ -24,9 +25,19 @@ using namespace Typed::Core;
 using namespace Typed::Core::Backend;
 
 ADT::Runtime::Object Walker::Return(
+    ProcedureRef procedure,
     VariableMap &stack,
     TreePtr body
 )
 {
+    // Make sure the procedure is allowed to return
+    if (procedure.return_type == ADT::Runtime::ObjectType::Void)
+    {
+        throw ADT::Exception::UnexpectedReturn(
+            body->line,
+            body->column
+        );
+    }
+
     return Expression(stack, body->children[0]);
 }
