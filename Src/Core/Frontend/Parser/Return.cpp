d6f147f3ee0d24f448c0a221fc4f9d757eb0eef7
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-21 17:36:30 -0500

    feat: implement parsing for return statements

---
+++ Src/Core/Frontend/Parser/Return.cpp
@@ -0,0 +1,39 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/21/26.
+//
+
+#include "Parser.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Frontend;
+using namespace Typed::Core::Frontend::Parser;
+
+void Machine::Return(TreePtr parent)
+{
+    // Allocate the return node
+    auto node = AllocateBase(
+        tokens.Peek(),
+        ADT::Lang::ASTType::Return
+    );
+
+    parent->children.PushBack(node);
+
+    // Parse the expression
+    Expression(node);
+}
