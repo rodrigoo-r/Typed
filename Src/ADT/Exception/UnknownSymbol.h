aa237dfc94beefbb4d0907511637d34666ce7b20
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-21 23:39:12 -0500

    feat: add UnknownSymbol exception

---
+++ Src/ADT/Exception/UnknownSymbol.h
@@ -0,0 +1,35 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#pragma once
+#include <Celery/Except/Base.h>
+
+#include "ADT/Core/Traceable.h"
+
+namespace Typed::ADT::Exception
+{
+    class UnknownSymbol :
+        public Celery::Except::Exception
+    {
+    public:
+        UnknownSymbol() :
+            Exception("Unknown symbol")
+        {}
+    };
+}
