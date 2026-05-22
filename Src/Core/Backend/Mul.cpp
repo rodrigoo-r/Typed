9005700faa9d8662c46e227d26d34a46967bb9f9
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-22 00:41:48 -0500

    feat: implement handling for arithmetic instructions

---
+++ Src/Core/Backend/Mul.cpp
@@ -0,0 +1,29 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/22/26.
+//
+
+#include "Walker.h"
+
+using namespace Typed;
+using namespace Typed::Core;
+using namespace Typed::Core::Backend;
+
+void Walker::Mul(VariableMap &stack, TreePtr body)
+{
+    ArithmeticBase(stack, body);
+}
