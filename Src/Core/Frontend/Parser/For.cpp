58eccdefef0f2c996b89de28641a32019dfe0d7a
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-23 17:08:00 -0500

    feat: enhance `For` loop parsing with variable declaration, range, and step support

--- Src/Core/Frontend/Parser/For.cpp
+++ Src/Core/Frontend/Parser/For.cpp
@@ -18,6 +18,8 @@
 //
 
 #include "Parser.h"
+#include "Support/Equality/TokenType.h"
+#include "Support/Stream/Extract.h"
 
 using namespace Typed;
 using namespace Typed::Core;
@@ -26,5 +28,43 @@ using namespace Typed::Core::Frontend::Parser;
 
 void Machine::For(TreePtr parent, BodyQueue &body_queue)
 {
+    auto &for_token = tokens.Peek();
+    auto ast = Allocate(ADT::Lang::ASTType::For);
+    parent->children.PushBack(ast);
 
+    auto &var_name = tokens.Peek();
+    Expect(ADT::Lang::TokenType::Identifier);
+    Expect(ADT::Lang::TokenType::As);
+    auto type = Type();
+
+    Expect(ADT::Lang::TokenType::In);
+    auto from = Support::Stream::Extract<
+        ADT::Lang::Token,
+        Support::Equality::TokenType
+    >(tokens, ADT::Lang::TokenType::To);
+
+    auto to = Support::Stream::Extract<
+        ADT::Lang::Token,
+        Support::Equality::TokenType
+    >(tokens, ADT::Lang::TokenType::Step);
+
+    auto step = Support::Stream::Extract<
+        ADT::Lang::Token,
+        Support::Equality::TokenType
+    >(tokens, ADT::Lang::TokenType::Do);
+
+    auto body = AllocateBase(for_token, ADT::Lang::ASTType::Body);
+
+    Expect(ADT::Lang::TokenType::Begin);
+    body_queue.emplace_back(body, for_token);
+
+    auto decl = AllocateBase(var_name, ADT::Lang::ASTType::Declare);
+    decl->children.PushBack(AllocateBase(var_name, ADT::Lang::ASTType::Identifier));
+    decl->children.PushBack(type);
+
+    ast->children.PushBack(decl);
+    Expression(ast, from);
+    Expression(ast, to);
+    Expression(ast, step);
+    ast->children.PushBack(body);
 }
