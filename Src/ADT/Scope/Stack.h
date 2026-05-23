819863631bf18a7a2d84fa9e92c17c9a54316e12
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-23 11:51:47 -0500

    feat: add scoped map data structure with hierarchical stack support

---
+++ Src/ADT/Scope/Stack.h
@@ -0,0 +1,135 @@
+/*
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/23/26.
+//
+
+#pragma once
+#include "ADT/Map/Dense.h"
+#include "Support/Allocator/Monotonic.h"
+
+namespace Typed::ADT::Scope
+{
+    template <
+        typename Key,
+        typename Value,
+        typename Hash = ankerl::unordered_dense::hash<Key>,
+        typename Equal = std::equal_to<Key>
+    >
+    struct Scope
+    {
+        Map::Dense<Key, Value, Hash, Equal> map{};
+        Scope *parent = nullptr;
+    };
+
+    template <
+        typename Key,
+        typename Value,
+        typename Hash = ankerl::unordered_dense::hash<Key>,
+        typename Equal = std::equal_to<Key>,
+        typename Allocator = Support::Allocator::Monotonic<
+            Scope<Key, Value, Hash, Equal>
+        >
+    >
+    class Stack
+    {
+        using InnerScope =
+            Scope<Key, Value, Hash, Equal>;
+
+        using InnerMap =
+            Map::Dense<Key, Value, Hash, Equal>;
+
+        InnerScope *top = nullptr;
+
+    public:
+        struct FakeIterator
+        {
+        private:
+            InnerScope *scope = nullptr;
+
+        public:
+            InnerMap::iterator it;
+
+            FakeIterator(
+                InnerScope *scope,
+                InnerMap::iterator it
+            ) :
+                scope(scope),
+                it(it)
+            {}
+
+            bool IsEnd()
+            {
+                return it == scope->map.end();
+            }
+        };
+
+        void PushScope()
+        {
+            auto scope = Allocator::Allocate();
+            scope->parent = top;
+            top = scope;
+        }
+
+        void PopScope()
+        {
+            Allocator::Deallocate(top);
+            top = top->parent;
+        }
+
+        FakeIterator Get(const Key &key)
+        {
+            auto scope = top;
+            while (scope != nullptr)
+            {
+                auto it = scope->map.find(key);
+                if (it != scope->map.end())
+                {
+                    return {
+                        scope,
+                        it
+                    };
+                }
+
+                scope = scope->parent;
+            }
+
+            // By this point, we should've searched all
+            // existing scopes
+            return {
+                scope,
+                scope->map.end()
+            };
+        }
+
+        void Set(const Key &key, const Value &value)
+        {
+            // Update if it exists first
+            auto it = Get(key);
+            if (!it.IsEnd())
+            {
+                it.it->second = value;
+            }
+            else
+            {
+                // Insert at the top stack if it doesn't exist
+                top->map.insert({
+                    key,
+                    value
+                });
+            }
+        }
+    };
+}
