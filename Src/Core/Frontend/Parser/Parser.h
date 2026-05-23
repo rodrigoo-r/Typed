faf9d1dc05aeb0b29d126b77da8cca6c981c3de9
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-23 17:09:09 -0500

    feat: implement `EndFor` method in `Parser` to validate and finalize `For` loop blocks

--- Src/Core/Frontend/Parser/Parser.h
+++ Src/Core/Frontend/Parser/Parser.h
@@ -88,7 +88,9 @@ namespace Typed::Core::Frontend::Parser
         void Expression(TreePtr body, TokenStreamView &input);
         void Return(TreePtr parent);
         void If(TreePtr parent, BodyQueue &body_queue);
+
         void For(TreePtr parent, BodyQueue &body_queue);
+        void EndFor(BodyQueue &body_queue);
 
         TreePtr ConditionGroup(TreePtr parent, BodyQueue &body_queue);
         void ElseIf(TreePtr parent, BodyQueue &body_queue);
