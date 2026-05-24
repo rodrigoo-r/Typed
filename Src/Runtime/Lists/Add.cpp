ff6ade4aa20a124a93e4868c656c8c213a386221
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-24 01:58:44 -0500

    feat: implement `Add` functionality and add `Lists` package in runtime

---
+++ Src/Runtime/Lists/Add.cpp
@@ -0,0 +1,56 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/24/26.
+//
+
+#include "ADT/Lang/AST.h"
+#include "ADT/List/Object.h"
+#include "Add.h"
+
+#include "ADT/Exception/MismatchedType.h"
+#include "Support/Printer/ASTPrinter.h"
+
+using namespace Typed;
+using namespace Typed::Runtime;
+using namespace Typed::Runtime::Lists;
+
+ADT::Runtime::Object Lists::Add(
+    ADT::List::Object &args,
+    ADT::Lang::AST *trace
+)
+{
+    auto &list = Support::Runtime::GetListObj(args[0]);
+
+    // Push all args to the list
+    for (auto i = 1; i < args.Size(); i++)
+    {
+        auto &arg = args[i];
+
+        // Make sure the object isn't void
+        if (arg.type == ADT::Runtime::ObjectType::Void)
+        {
+            throw ADT::Exception::MismatchedType(
+                trace->line,
+                trace->column
+            );
+        }
+
+        list->PushBack(arg);
+    }
+
+    return {};
+}
---
+++ Src/Runtime/Lists/Package.h
@@ -0,0 +1,42 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/24/26.
+//
+
+#pragma once
+
+#include "ADT/Map/Procedure.h"
+#include "Add.h"
+
+namespace Typed::Runtime::Lists
+{
+    inline ADT::Map::Procedure Package = {
+        {
+            "List_Add",
+            {
+                {
+                    {"f", ADT::Runtime::ObjectType::List},
+                    {"_", ADT::Runtime::ObjectType::Any},
+                },
+                nullptr,
+                ADT::Runtime::ObjectType::Void,
+                true,
+                Add
+            }
+        }
+    };
+}
