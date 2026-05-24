7d93cb9fa1fdb1e6b08d10b3bf4f7c2d4c53588b
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-24 10:54:31 -0500

    feat: implement `Access` functionality in `Lists` package in runtime

---
+++ Src/Runtime/Lists/Access.cpp
@@ -0,0 +1,46 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/24/26.
+//
+
+#include "ADT/List/Object.h"
+#include "Access.h"
+
+#include "ADT/Exception/OutOfBounds.h"
+
+using namespace Typed;
+using namespace Typed::Runtime;
+using namespace Typed::Runtime::Lists;
+
+ADT::Runtime::Object Lists::Access(
+    ADT::List::Object &args,
+    ADT::Lang::AST *trace
+)
+{
+    auto &list = Support::Runtime::GetListObj(args[0]);
+    auto &idx = Support::Runtime::GetIntObj(args[1]);
+
+    if (idx >= list->Size())
+    {
+        throw ADT::Exception::OutOfBounds(
+            trace->line,
+            trace->column
+        );
+    }
+
+    return (*list)[idx];
+}
