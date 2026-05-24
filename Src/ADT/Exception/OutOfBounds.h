2626ec6a4477d66feb918bada52a486273bf1b38
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-24 07:50:50 -0500

    feat: add `OutOfBounds` exception to `ADT` package

---
+++ Src/ADT/Exception/OutOfBounds.h
@@ -0,0 +1,40 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+
+//
+// Created by Rodrigo on 5/18/26.
+//
+
+#pragma once
+#include <Celery/Except/Base.h>
+
+#include "ADT/Core/Traceable.h"
+
+namespace Typed::ADT::Exception
+{
+    class OutOfBounds :
+        public Celery::Except::Exception,
+        public Core::Traceable
+    {
+    public:
+        OutOfBounds(
+            Celery::Trait::VeryLarge line,
+            Celery::Trait::VeryLarge column
+        ) :
+            Exception("Index out of bounds"),
+            Traceable{line, column}
+        {}
+    };
+}
