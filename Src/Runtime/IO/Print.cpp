6f8155235a53815a835a47fa808975a60468dbfc
rodrigoo-r <rodrigo@zelixlang.dev>
2026-05-24 00:49:52 -0500

    feat: add `PrintObject` and specialized printing functions for improved type-based I/O handling

--- Src/Runtime/IO/Print.cpp
+++ Src/Runtime/IO/Print.cpp
@@ -46,6 +46,129 @@ ADT::Runtime::Object &GetArg(
     return obj;
 }
 
+void PrintString(ADT::Runtime::Object &obj)
+{
+    // Special case: Owned strings
+    if (obj.type == ADT::Runtime::ObjectType::OwnedString)
+    {
+        auto &val = Support::Runtime::GetOwnedStrObj(obj);
+        Celery::Io::Print(val);
+        return;
+    }
+
+    auto &val = Support::Runtime::GetStrObj(obj);
+    Celery::Io::Print(val);
+}
+
+void PrintFloat(ADT::Runtime::Object &obj)
+{
+    auto &val = Support::Runtime::GetFloatObj(obj);
+    Celery::Io::Print(val);
+}
+
+void PrintInt(ADT::Runtime::Object &obj)
+{
+    auto &val = Support::Runtime::GetIntObj(obj);
+    Celery::Io::Print(val);
+}
+
+void PrintBool(ADT::Runtime::Object &obj)
+{
+    auto &val = Support::Runtime::GetBoolObj(obj);
+    Celery::Io::Print(val);
+}
+
+void PrintList(ADT::Runtime::Object &obj);
+void PrintDictionary(ADT::Runtime::Object &obj);
+void PrintObject(ADT::Runtime::Object &obj);
+
+void PrintDictionary(ADT::Runtime::Object &obj)
+{
+    auto &val = Support::Runtime::GetDictionaryObj(obj);
+
+    Celery::Io::Print("Dict(");
+
+    auto last = val.size() - 1;
+    Celery::Trait::VeryLarge i = 0;
+
+    for (auto &pair : val)
+    {
+        PrintObject(pair.first);
+        Celery::Io::Print(" : ");
+        PrintObject(pair.second);
+
+        if (i != last)
+        {
+            Celery::Io::Print(", ");
+        }
+
+        i++;
+    }
+
+    Celery::Io::Print(')');
+}
+
+void PrintList(ADT::Runtime::Object &obj)
+{
+    Celery::Io::Print("List(");
+    auto &val = Support::Runtime::GetListObj(obj);
+
+    auto last = val.Size() - 1;
+    for (auto i = 0; i < val.Size(); i++)
+    {
+        PrintObject(obj);
+
+        if (i != last)
+            Celery::Io::Print(", ");
+    }
+
+    Celery::Io::Print(')');
+}
+
+void PrintObject(ADT::Runtime::Object &obj)
+{
+    switch (obj.type)
+    {
+        case ADT::Runtime::ObjectType::String:
+        {
+            PrintString(obj);
+            break;
+        }
+
+        case ADT::Runtime::ObjectType::Float:
+        {
+            PrintFloat(obj);
+            break;
+        }
+
+        case ADT::Runtime::ObjectType::Integer:
+        {
+            PrintInt(obj);
+            break;
+        }
+
+        case ADT::Runtime::ObjectType::Boolean:
+        {
+            PrintBool(obj);
+            break;
+        }
+
+        case ADT::Runtime::ObjectType::List:
+        {
+            PrintList(obj);
+            break;
+        }
+
+        case ADT::Runtime::ObjectType::Dictionary:
+        {
+            PrintDictionary(obj);
+            break;
+        }
+
+        default: break;
+    }
+}
+
 ADT::Runtime::Object IO::Print(
     ADT::List::Object &args,
     ADT::Lang::AST *trace
@@ -66,12 +189,13 @@ ADT::Runtime::Object IO::Print(
         {
             auto f = fmt[i + 1];
             auto can_continue = true;
+            ADT::Runtime::Object &arg = args.Front();
 
             switch (f)
             {
                 case 'S':
                 {
-                    auto &arg = GetArg(args, pos, line, col);
+                    arg = GetArg(args, pos, line, col);
                     Support::Runtime::TypeCheck(
                         ADT::Runtime::ObjectType::String,
                         arg.type,
@@ -79,25 +203,13 @@ ADT::Runtime::Object IO::Print(
                         col
                     );
 
-                    // Special case: Owned strings
-                    if (arg.type == ADT::Runtime::ObjectType::OwnedString)
-                    {
-                        auto &val = Support::Runtime::GetOwnedStrObj(arg);
-                        can_continue = false;
-                        Celery::Io::Print(val);
-                        break;
-                    }
-
-                    auto &val = Support::Runtime::GetStrObj(arg);
                     can_continue = false;
-                    Celery::Io::Print(val);
-
                     break;
                 }
 
                 case 'F':
                 {
-                    auto &arg = GetArg(args, pos, line, col);
+                    arg = GetArg(args, pos, line, col);
                     Support::Runtime::TypeCheck(
                         ADT::Runtime::ObjectType::Float,
                         arg.type,
@@ -105,16 +217,13 @@ ADT::Runtime::Object IO::Print(
                         col
                     );
 
-                    auto &val = Support::Runtime::GetFloatObj(arg);
                     can_continue = false;
-                    Celery::Io::Print(val);
-
                     break;
                 }
 
                 case 'I':
                 {
-                    auto &arg = GetArg(args, pos, line, col);
+                    arg = GetArg(args, pos, line, col);
                     Support::Runtime::TypeCheck(
                         ADT::Runtime::ObjectType::Integer,
                         arg.type,
@@ -122,16 +231,13 @@ ADT::Runtime::Object IO::Print(
                         col
                     );
 
-                    auto &val = Support::Runtime::GetIntObj(arg);
                     can_continue = false;
-                    Celery::Io::Print(val);
-
                     break;
                 }
 
                 case 'B':
                 {
-                    auto &arg = GetArg(args, pos, line, col);
+                    arg = GetArg(args, pos, line, col);
                     Support::Runtime::TypeCheck(
                         ADT::Runtime::ObjectType::Boolean,
                         arg.type,
@@ -139,10 +245,35 @@ ADT::Runtime::Object IO::Print(
                         col
                     );
 
-                    auto &val = Support::Runtime::GetBoolObj(arg);
                     can_continue = false;
-                    Celery::Io::Print(val);
+                    break;
+                }
 
+                case 'L':
+                {
+                    arg = GetArg(args, pos, line, col);
+                    Support::Runtime::TypeCheck(
+                        ADT::Runtime::ObjectType::List,
+                        arg.type,
+                        line,
+                        col
+                    );
+
+                    can_continue = false;
+                    break;
+                }
+
+                case 'D':
+                {
+                    arg = GetArg(args, pos, line, col);
+                    Support::Runtime::TypeCheck(
+                        ADT::Runtime::ObjectType::Dictionary,
+                        arg.type,
+                        line,
+                        col
+                    );
+
+                    can_continue = false;
                     break;
                 }
 
@@ -152,6 +283,7 @@ ADT::Runtime::Object IO::Print(
             // Skip the next character if we found a format
             if (!can_continue)
             {
+                PrintObject(arg);
                 i++;
                 continue;
             }
