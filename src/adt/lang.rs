04b98a74e3134182ce5c29a0855f0b3889b63f17
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-19 18:09:08 -0500

    refactor: enhance `lang.rs` with refined type definitions and derive `Clone` for structs

--- src/adt/lang.rs
+++ src/adt/lang.rs
@@ -23,9 +23,11 @@ pub type BaseChildAST<'a> = RefCell<AST<'a>>;
 pub type ChildAST<'a> = Rc<BaseChildAST<'a>>;
 pub type ASTChildren<'a> = RefCell<Vec<ChildAST<'a>>>;
 
-pub type RuntimeArguments<'a> = &'a List<'a>;
+pub type RuntimeArgument<'a> =
+    (&'a str, Object<'a>);
+pub type RuntimeArguments<'a> = &'a Vec<RuntimeArgument<'a>>;
 pub type NativeProcedure<'a> =
-    fn(RuntimeArguments<'a>) -> ExecutionResult<'a>;
+    fn(RuntimeArguments<'a>, &'a ChildAST<'a>) -> ExecutionResult<'a>;
 
 #[derive(Debug, Clone)]
 pub struct AST<'a> {
@@ -36,7 +38,7 @@ pub struct AST<'a> {
     pub column: usize
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Kind {
     String,
     Integer,
@@ -46,16 +48,16 @@ pub enum Kind {
     List
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Argument<'a> {
     pub name: &'a str,
     pub kind: Kind
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Procedure<'a> {
     pub arguments: Vec<Argument<'a>>,
-    pub body: ChildAST<'a>,
+    pub body: Option<ChildAST<'a>>,
     pub variadic: bool,
     pub native: Option<NativeProcedure<'a>>
 }
