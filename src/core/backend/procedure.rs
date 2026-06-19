8c5438cda780729df7675bc4ba5e0a0abe72541a
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-19 00:21:19 -0500

    feat: add procedure execution function with argument scoping in `procedure.rs`

---
+++ src/core/backend/procedure.rs
@@ -0,0 +1,41 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+use crate::adt::lang::Procedure;
+use crate::adt::runtime::Object;
+use crate::adt::variable::ScopedStack;
+
+pub fn execute<'a>(
+    procedure: &Procedure<'a>,
+    given_args: Vec<(&'a str, Object<'a>)>
+) {
+    let mut stack = ScopedStack::new(None);
+
+    // Add all arguments
+    for (name, arg) in given_args.iter() {
+        stack.push(name, arg);
+    }
+
+    let body = procedure.body.borrow();
+    let children = body.children.borrow();
+
+    for statement in children.iter() {
+        let statement = statement.borrow();
+
+        match statement.rule {
+
+            _ => {}
+        }
+    }
+}
