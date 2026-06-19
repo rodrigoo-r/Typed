a940f64f536fd9c0a5682f50bee1490d4d8656a2
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-19 11:58:01 -0500

    feat: add `result` module with `ExecutionResult` type alias for standardized execution outcomes

---
+++ src/adt/result.rs
@@ -0,0 +1,21 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+use crate::adt::error::ExecutionError;
+use crate::adt::runtime::Object;
+
+pub type ExecutionResult<'a> = Result<
+    Object<'a>,
+    ExecutionError
+>;
