795dee95714bad40eade94f4817d2fa19e1ec234
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-18 22:38:15 -0500

    feat: derive `Debug` for `Kind`, `Argument`, `Procedure`, and `File` structs to improve debugging capability

--- src/adt/lang.rs
+++ src/adt/lang.rs
@@ -26,6 +26,7 @@ pub struct AST<'a> {
     pub column: usize
 }
 
+#[derive(Debug)]
 pub enum Kind {
     String,
     Integer,
@@ -35,16 +36,19 @@ pub enum Kind {
     List
 }
 
+#[derive(Debug)]
 pub struct Argument<'a> {
     pub name: &'a str,
     pub kind: Kind
 }
 
+#[derive(Debug)]
 pub struct Procedure<'a> {
     pub arguments: Vec<Argument<'a>>,
     pub body: Rc<RefCell<AST<'a>>>
 }
 
+#[derive(Debug)]
 pub struct File<'a> {
     pub imports: Vec<String>,
     pub procedures: HashMap<&'a str, Procedure<'a>>
