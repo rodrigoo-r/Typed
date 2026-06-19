0ea1d9409ea3cb4d60a4c70e6c702be5db4b4c2f
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-19 18:30:25 -0500

    refactor: update `execute` to pass `ProcedureArguments` and handle `main` body directly

--- src/core/backend/mod.rs
+++ src/core/backend/mod.rs
@@ -14,12 +14,19 @@
 */
 mod procedure;
 
-use crate::adt::lang::File;
+use crate::adt::lang::{File, ProcedureArguments, RuntimeArguments};
 use crate::adt::result::ExecutionResult;
 
-pub fn execute(file: File) -> ExecutionResult {
+pub fn execute<'a>(
+    file: &'a File<'a>,
+    empty_args: ProcedureArguments<'a>
+) -> ExecutionResult<'a> {
     // Get the main procedure
     let main = file.procedures.get("Main").unwrap();
 
-    procedure::execute(main, Vec::new())
+    procedure::execute(
+        main.body.as_ref().unwrap(),
+        main,
+        &empty_args
+    )
 }
