b2199d2bc011a827b314c2516a72792edf9dad75
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-19 18:09:23 -0500

    refactor: update `execute` to support `ChildAST` and `RuntimeArguments`, and handle native procedures

--- src/core/backend/procedure.rs
+++ src/core/backend/procedure.rs
@@ -12,14 +12,16 @@
  * #                                                     # *
  * #-----------------------------------------------------# *
 */
-use crate::adt::lang::Procedure;
+use crate::adt::lang::{ChildAST, Procedure, RuntimeArguments, AST};
 use crate::adt::result::ExecutionResult;
 use crate::adt::runtime::Object;
 use crate::adt::variable::ScopedStack;
+use crate::core::frontend;
 
 pub fn execute<'a>(
+    trace: &'a ChildAST<'a>,
     procedure: &Procedure<'a>,
-    given_args: Vec<(&'a str, Object<'a>)>
+    given_args: RuntimeArguments<'a>
 ) -> ExecutionResult<'a> {
     let mut stack = ScopedStack::new(None);
 
@@ -27,18 +29,43 @@ pub fn execute<'a>(
     for (name, arg) in given_args.iter() {
         stack.push(name, arg);
     }
+    
+    // Call native procedures if needed
+    if procedure.native.is_some() {
+        return procedure.native.as_ref().unwrap()(given_args, trace);
+    }
 
-    let body = procedure.body.borrow();
+    let body = procedure.body.as_ref();
+    let body = body.unwrap().borrow();
     let children = body.children.borrow();
 
     for statement in children.iter() {
         let statement = statement.borrow();
 
         match statement.rule {
+            frontend::parser::Rule::Expression => {
+
+            }
+
+            frontend::parser::Rule::For => {
+
+            }
+
+            frontend::parser::Rule::While => {
+
+            }
+
+            frontend::parser::Rule::Declare => {
+
+            }
+
+            frontend::parser::Rule::Condition_Group => {
+
+            }
 
             _ => {}
         }
     }
-    
+
     Ok(Object::Void)
 }
