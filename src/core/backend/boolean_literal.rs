1980b519fec635679cdaf33bf2da63b54204bc0c
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-20 14:15:18 -0500

    feat: add `boolean_literal.rs` to handle boolean literal evaluation in AST

---
+++ src/core/backend/boolean_literal.rs
@@ -0,0 +1,41 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+use crate::adt::error::{ErrorKind, ExecutionError};
+use crate::adt::lang::AST;
+use crate::adt::result::ExecutionResult;
+use crate::adt::runtime::{HashableObject, Object};
+
+pub fn evaluate<'a>(
+    child: &AST<'a>
+) -> ExecutionResult<'a> {
+    let value = child.value.unwrap();
+
+    if value.eq("True") {
+        return Ok(Object::Hashable(HashableObject::Boolean(true)));
+    }
+
+    if value.eq("False") {
+        return Ok(Object::Hashable(HashableObject::Boolean(false)));
+    }
+
+    Err(
+        ExecutionError{
+            kind: ErrorKind::MisformedLiteral,
+            message: "Invalid boolean literal",
+            line: child.line,
+            column: child.column
+        }
+    )
+}
