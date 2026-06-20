1cbb29047df869b98e102be4588b34e33364d8fc
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-20 14:15:29 -0500

    feat: add `integer_literal.rs` to handle integer literal evaluation in AST

---
+++ src/core/backend/integer_literal.rs
@@ -0,0 +1,46 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+use crate::adt::error::{ErrorKind, ExecutionError};
+use crate::adt::lang::AST;
+use crate::adt::result::ExecutionResult;
+use crate::adt::runtime::{HashableObject, Object};
+
+pub fn evaluate<'a>(
+    child: &AST<'a>
+) -> ExecutionResult<'a> {
+    let value = child.value.unwrap();
+    
+    // Parse the value
+    let value = value.parse::<isize>();
+    
+    if value.is_err() {
+        return Err(
+            ExecutionError{
+                kind: ErrorKind::MisformedLiteral,
+                message: "Invalid integer literal",
+                line: child.line,
+                column: child.column
+            }
+        );
+    }
+    
+    Ok(
+        Object::Hashable(
+            HashableObject::Integer(
+                value.unwrap()
+            )
+        )
+    )
+}
