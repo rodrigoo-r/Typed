6979e221bc8b28149aa0b152d2bde86692a2bb10
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-20 15:03:46 -0500

    refactor: replace `ScopedStack` with `NestedStack` in `call.rs` for improved stack handling

--- src/core/backend/call.rs
+++ src/core/backend/call.rs
@@ -15,21 +15,21 @@
 use crate::adt::error::{ErrorKind, ExecutionError};
 use crate::adt::lang::{File, RuntimeArguments, AST};
 use crate::adt::result::ExecutionResult;
-use crate::adt::variable::ScopedStack;
+use crate::adt::variable::NestedStack;
 use crate::core::backend::{expression, procedure};
 
-pub fn evaluate<'a, 'stack>(
+pub fn evaluate<'a>(
     file: &'a File<'a>,
     child: &AST<'a>,
-    stack: &'stack ScopedStack<'a, 'stack>
+    stack: &NestedStack<'a>
 ) -> ExecutionResult<'a> {
     let children = child.children.borrow();
     let name = children.get(0).unwrap();
     let name = name.borrow().value.unwrap();
-    
+
     // Find the procedure
     let procedure = file.procedures.get(name);
-    
+
     if procedure.is_none() {
         return Err(
             ExecutionError{
@@ -40,15 +40,15 @@ pub fn evaluate<'a, 'stack>(
             }
         );
     }
-    
+
     let procedure = procedure.unwrap();
     let mut args: RuntimeArguments = RuntimeArguments::new();
-    
+
     // Add arguments if needed
     if children.len() > 1 {
         let ast_args = children.get(1).unwrap();
         let ast_args = ast_args.borrow();
-        
+
         for arg in ast_args.children.borrow().iter() {
             let arg = arg.borrow();
             args.push(
@@ -56,7 +56,7 @@ pub fn evaluate<'a, 'stack>(
             );
         }
     }
-    
+
     // Execute the procedure
     procedure::execute(file, child, procedure, args)
 }
