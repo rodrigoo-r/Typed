7f9f38999a3039ca80d03ce10d128eba5068e195
rodrigoo-r <rodrigo@zelixlang.dev>
2026-06-20 21:32:11 -0500

    refactor: remove trailing whitespace in `kind.rs` for cleaner formatting

--- src/support/runtime/kind.rs
+++ src/support/runtime/kind.rs
@@ -25,7 +25,7 @@ pub fn check_kinds(
     if expected != actual {
         return Err(ExecutionError::mismatched_types(trace));
     }
-    
+
     Ok(())
 }
 
@@ -42,27 +42,27 @@ pub fn check_kind<'a>(
         Object::Hashable(HashableObject::Float(_)) => {
             check_kinds(expected, Kind::Float, trace)
         }
-        
+
         Object::Hashable(HashableObject::Boolean(_)) => {
             check_kinds(expected, Kind::Boolean, trace)
         }
-        
+
         Object::Hashable(HashableObject::String(_)) => {
             check_kinds(expected, Kind::String, trace)
         }
-        
+
         Object::NonHashable(NonHashableObject::List(_)) => {
             check_kinds(expected, Kind::List, trace)
         }
-        
+
         Object::NonHashable(NonHashableObject::Dictionary(_)) => {
             check_kinds(expected, Kind::Dictionary, trace)
         }
-        
+
         Object::Void => {
             Err(ExecutionError::mismatched_types(trace))
         }
-        
+
         Object::Any(_) => {
             Ok(())
         }
@@ -109,31 +109,33 @@ pub fn check_obj_kind<'a>(
     }
 }
 
-pub fn convert_kind<'a>(decl: &AST<'a>) 
-    -> (&'a str, Kind) 
+pub fn convert_kind<'a>(decl: &AST<'a>)
+    -> (&'a str, Kind)
 {
     let children = decl.children.borrow();
     let name = children.get(0).unwrap().borrow();
     let name = name.value.unwrap();
-    
+
     let kind = children.get(1).unwrap().borrow();
+    let kind = kind.children.borrow();
+    let kind = kind.get(0).unwrap().borrow();
 
     (
         name,
         match kind.rule {
-            crate::core::frontend::parser::Rule::Integer => 
+            crate::core::frontend::parser::Rule::Integer =>
                 Kind::Integer,
-            crate::core::frontend::parser::Rule::Float => 
+            crate::core::frontend::parser::Rule::Float =>
                 Kind::Float,
-            crate::core::frontend::parser::Rule::String => 
+            crate::core::frontend::parser::Rule::String =>
                 Kind::String,
-            crate::core::frontend::parser::Rule::Boolean => 
+            crate::core::frontend::parser::Rule::Boolean =>
                 Kind::Boolean,
-            crate::core::frontend::parser::Rule::Dictionary => 
+            crate::core::frontend::parser::Rule::Dictionary =>
                 Kind::Dictionary,
-            crate::core::frontend::parser::Rule::List => 
+            crate::core::frontend::parser::Rule::List =>
                 Kind::List,
-            
+
             _ => unreachable!(),
         }
     )
