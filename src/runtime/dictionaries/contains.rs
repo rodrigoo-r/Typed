cae9a3df2bf27fb060e132df53cec30eec594db6
rodrigoo-r <rodrigo@zelixlang.dev>
2026-09-06 21:58:45 -0500

    feat: add contains function for dictionary to check key existence

---
+++ src/runtime/dictionaries/contains.rs
@@ -0,0 +1,51 @@
+/*
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #                           Typed                     # *
+ * #                   A text formatting DSL             # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+ * #                                                     # *
+ * #         Created by Rodrigo R. & Contributors        # *
+ * #         Released under the Apache License 2.0       # *
+ * #            Check LICENSE.MD for more info           # *
+ * #                                                     # *
+ * #-----------------------------------------------------# *
+*/
+use crate::adt::error::RuntimeError;
+use crate::adt::lang::{RuntimeArguments, AST};
+use crate::adt::result::ExecutionTupleResult;
+use crate::adt::runtime::{HashableObject, Object};
+use crate::support::runtime::execution::continue_execution;
+use crate::support::runtime::object::get_dictionary;
+
+pub fn contains<'a>(
+    args: RuntimeArguments<'a>,
+    trace: &AST<'a>
+)
+    -> ExecutionTupleResult<'a>
+{
+    let dict_obj = args.get(0).unwrap();
+    let key = args.get(1).unwrap();
+    
+    if let Object::NonHashable(_) = key {
+        return Err(
+            RuntimeError::key_not_found(trace)
+        );
+    }
+
+    let 
+        Object::Hashable(key) 
+    = key else { unreachable!() };
+    let dict = get_dictionary(dict_obj, trace)?;
+
+    let dict = dict.borrow();
+    
+    continue_execution(
+        Object::Hashable(
+            HashableObject::Boolean(
+                dict.contains_key(&key)
+            )
+        )
+    )
+}
--- src/runtime/dictionaries/mod.rs
+++ src/runtime/dictionaries/mod.rs
@@ -17,6 +17,7 @@ pub mod set;
 pub mod remove;
 pub mod size;
 pub mod keys;
+pub mod contains;
 
 use crate::adt::lang::{ASTValue, Argument, Kind, Procedure};
 use crate::adt::runtime::PackageDictionary;
@@ -67,7 +68,7 @@ pub fn get_package<'a>() -> PackageDictionary<'a> {
                         kind: Kind::Dictionary,
                     }
                 );
-                
+
                 vec
             },
             native: Some(keys::keys),
@@ -141,6 +142,35 @@ pub fn get_package<'a>() -> PackageDictionary<'a> {
     );
 
     dict.insert(
+        "Dictionary_Contains",
+        Procedure{
+            variadic: false,
+            body: None,
+            arguments: {
+                let mut vec = vec![];
+
+                vec.push(
+                    Argument{
+                        name: ASTValue::Borrowed("dict"),
+                        kind: Kind::Dictionary,
+                    }
+                );
+
+                vec.push(
+                    Argument{
+                        name: ASTValue::Borrowed("key"),
+                        kind: Kind::Any,
+                    }
+                );
+
+                vec
+            },
+            native: Some(contains::contains),
+            ret: None
+        }
+    );
+
+    dict.insert(
         "Dictionary_Size",
         Procedure{
             variadic: false,
@@ -161,6 +191,6 @@ pub fn get_package<'a>() -> PackageDictionary<'a> {
             ret: None
         }
     );
-    
+
     dict
 }
